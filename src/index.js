1b9bbcaf386474285c64a7ed1f5cb3b6084b7758
scotty <portrayme@mac.com>
2018-01-19 18:53:36 -0800

    we're in a transitional moment with index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,62 @@
 window.m = require('mithril')
 const State = require('./model/State')
-const Player = require('@vimeo/player')
+const Menu = require('./components/Menu/Menu')
+const Lecture = require('./components/Lecture/Lecture')
 
 require('./globals.sass')
 
-const ViewModel = {
-	mainClass: () => {
-		const classes = []
-		if (State.menuOpen) classes.push('menuOpen')
-		if (State.labOpen) classes.push('labOpen')
-		return classes.join(' ')
-	}
+function classListForMain() {
+	const classes = []
+	if (State.menuOpen) classes.push('menuOpen')
+	if (State.labOpen) classes.push('labOpen')
+	return classes.join(' ')
 }
 
 const App = {
-	oncreate: () => {
-		State.player = new Player('videoContainer', {id: 251386037})
-	},
 	view: () => {
 		const chapter = State.chapters[State.currentChapterIndex]
-		const flem = chapter.flems[State.currentFlemIndex]
-		const mainClass = ViewModel.mainClass()
+		const flem = chapter.flems ? chapter.flems[State.currentFlemIndex] : {}
+		const welcomeVisible = State.currentChapterIndex === 0
+		const mainClass = classListForMain()
 		
 		return m('main'
 			, { class: mainClass }
-			, m('menu'
-				, m('button.expander.show-menu', { onclick: State.toggleView.bind(null, 'menu')})
-			)
+			, m(Menu)
 			, m('.wrapper'
-				, m('section.lecture'
-					, m('h1.center', 'Mithril 0-60')
-					, m('#videoContainer')
-					, m('button.expander.show-lab', { onclick: State.toggleView.bind(null, 'lab')})
+				, m(Lecture, { 
+					canPlay: State.canPlay, 
+					welcomeVisible: welcomeVisible, 
+					expandFn: State.toggleView 
+				})
+				, m('section.lab'
+					, m('.resources'
+						, m('h5', 'Practicum')
+						, chapter.notes.split('|').map(function (note) {
+							return m('h4.notes', m.trust(note)) 
+						})
+						, m('ul.sandboxes'
+							, m('h5', 'Sandboxes')
+							, chapter.flems.map((fl, idx) => {
+								return m('h4'
+									, {
+										class: idx === State.currentFlemIndex ? 'current' : '',
+										onclick: State.setFlem.bind(null, idx) 
+									}
+									, fl.label
+								)
+							})
+						)
+						, m('ul.links'
+							, m('h5', 'Resources')
+							, chapter.links.map((link) => {
+								return m('li', m('a[target=_blank]', { href: link.url }, link.text))
+							})
+						)
+					)
+					, m('.flems'
+						, m('iframe', { src: 'http://tinyurl.com/' + flem.url })
+					)
 				)
-				, m('section.lab', 'bar')
 			)
 		)
 	}
