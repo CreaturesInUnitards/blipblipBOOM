edfd6e9c7a4f360d6a59e6e85563513fd1671106
scotty <portrayme@mac.com>
2018-01-19 18:50:45 -0800

    move app data into Data.js

---
+++ src/model/Data.js
@@ -0,0 +1,44 @@
+module.exports = [
+	{
+		name: 'Welcome',
+		notes: "Here you will see suggestions for activities related to your current chapter in the series.|" +
+		"Click the sandboxes for various starting points, then experiment with coding ideas and see your results in real time on the right.|" +
+		"(<i>Sandboxes are implemented using Rasmus Porsager's excellent Flems.io</i>)",
+		links: [
+			{ text: 'Official Mithril.js site', url: 'https://mithril.js.org' },
+			{ text: 'Mithril on GitHub', url: 'https://github.com/MithrilJS/mithril.js' },
+			{ text: 'Official Gitter channel', url: 'https://gitter.im/mithriljs/mithril.js' },
+			{ text: 'Mithril 10-minute guide', url: 'https://mithril.js.org/index.html' },
+			{ text: 'Official Mithril tutorial', url: 'https://mithril.js.org/simple-application.html' },
+			{ text: 'Flems.io by @porsager', url: 'https://flems.io' },
+			{ text: '@porsager on GitHub', url: 'https://github.com/porsager' },
+		],
+		flems: [
+			{ label: '"Hello world" in HTML :)', url: 'y9da38m6' },
+			{ label: '"Hello world" in vanilla JS', url: 'y8vhu2vg' }
+		]
+	},
+	{
+		name: 'Hello, World!',
+		id: 251555927,
+		notes: "We used two different methods to put text on the page: <code>m.render</code> and <code>m.mount</code>.|See what kind of damage you can do with each of them in the sandboxes.",
+		links: [
+			{ text: 'm.render API reference', url: 'https://mithril.js.org/render.html' },
+			{ text: 'm.mount API reference', url: 'https://mithril.js.org/mount.html' },
+		],
+		flems: [
+			{ label: "Using m.render", url: 'y7zau2jh' },
+			{ label: "Using m.mount", url: 'ybq58myu' }
+		]
+	},
+	{
+		name: 'Hyperscript, Components, and Auto-Redraw',
+		id: 140939541,
+		notes: "",
+		links: [],
+		flems: [
+			{ label: "Using m.render", url: 'y7zau2jh' },
+			{ label: "Using m.mount", url: 'ybq58myu' }
+		]
+	}
+]
