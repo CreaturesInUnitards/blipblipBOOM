2a7291b1502ea38b034750a8bb6a59b701658882
scotty <portrayme@mac.com>
2018-01-19 18:49:30 -0800

    Introducing: Welcome (kind of a silly component now. It made more sense yesterday.)

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,16 @@
+/***********************************
+*
+* Welcome
+*
+***********************************/
+require("./Welcome.sass")
+
+
+module.exports = {
+	view: (vnode) => {
+		return m('#welcome'
+			, { style: { display: vnode.attrs.visible ? 'flex' : 'none'} }
+			, m('.logo', m('i', 'm') , '(', m('i', ' Mithril, { 0-60 } '), ')')
+		)
+	}
+}
