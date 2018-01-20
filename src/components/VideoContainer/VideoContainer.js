d7a8362dbd976fc2014d438218190088bfc5acc1
scotty <portrayme@mac.com>
2018-01-19 18:48:47 -0800

    Introducing: VideoContainer

---
+++ src/components/VideoContainer/VideoContainer.js
@@ -0,0 +1,18 @@
+/***********************************
+*
+* VideoContainer
+*
+***********************************/
+require("./VideoContainer.sass")
+
+module.exports = {
+	onbeforeremove: (vnode) => {
+		vnode.dom.classList.add("exit")
+		return new Promise((resolve) => {
+			setTimeout(resolve, 800)
+		})
+	},
+	view: (vnode) => {
+		return m('#videoContainer', vnode.children)
+	}
+}
