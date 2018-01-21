cf498510a8b7aab4bb4fe52647add9f270bfbea0
scotty <portrayme@mac.com>
2018-01-21 05:52:50 -0800

    add cuepoints to Player video in/out animation tweaks

--- src/model/State.js
+++ src/model/State.js
@@ -2,8 +2,10 @@ const Player = require('@vimeo/player')
 const VIDEO_CONTAINER_ID = 'videoContainer'
 
 const State = {
-	firstRun_menu: false,
-	firstRun_lab: false,
+	firstRun: {
+		menu: true,
+		lab: true
+	},
 	chapters: require('./Data'),
 	canPlay: true,
 	currentChapterIndex: 0,
@@ -12,15 +14,17 @@ const State = {
 	labOpen: false,
 	player: null,
 	toggleView: function(w) {
+		State.firstRun[w] = false
 		const prop = w + 'Open'
 		State[prop] = !State[prop]
 		
 		if (w === 'lab' && State.player) State.player.pause()
 	},
-	setChapter: (n) => {
+	setChapter: (n, e) => {
 		State.currentChapterIndex = n
 		State.setFlem(0)
 		State.loadChapter(State.chapters[n].id)
+		State.labOpen = e.target.classList.contains('practicum') 
 	},
 	setFlem: (n) => {
 		State.currentFlemIndex = n
@@ -37,24 +41,32 @@ const State = {
 		})
 		
 		player.on('loaded', function() {
-			console.log("video loaded")
+			const chapter = State.chapters[State.currentChapterIndex]
+			let cuepointCount = 0
+			chapter.flems.forEach(function(flem){
+				if (flem.cuepoint) player.addCuePoint(flem.cuepoint, { idx: cuepointCount++ })
+			})
+
+			document.getElementById(VIDEO_CONTAINER_ID).classList.add('enter')
+
+		})
+		
+		player.on('cuepoint', function(notification){
+			State.setFlem(notification.data.idx)
+			State.toggleView('lab')
+			m.redraw()
 		})
 	},
 	loadChapter: (id) => {
 		State.canPlay = false
 
-		if (id) {
-			setTimeout(() => {
-				State.canPlay = true
-				m.redraw()
+		if (!id) return
 
-				setTimeout(() => {
-					State.setupPlayer(id)
-					document.getElementById(VIDEO_CONTAINER_ID).classList.add('enter')
-				}, 800)
-			}, 100)
-		}
-
+		setTimeout(() => {
+			State.canPlay = true
+			m.redraw()
+			setTimeout(State.setupPlayer.bind(null, id), 100)
+		}, 100)
 	}
 }
 
