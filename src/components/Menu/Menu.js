6968981ebc663d5d839e5c73c1e3b1dad53a4312
scotty <portrayme@mac.com>
2018-01-21 05:48:17 -0800

    Introducing: destination type in Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,21 +9,24 @@ module.exports = {
 	view: () => {
 		return m('menu'
 			, State.chapters.map((chapter, cidx) => {
+				const isVideo = cidx > 0
 				return m('.chapter'
-					, {
-						class: cidx === State.currentChapterIndex ? 'current' : '',
-						onclick: State.setChapter.bind(null, cidx)
-					}
-					, m(''
-						, cidx > 0 && m('h4.chapter-num', cidx + '.')
-						, m('h4.chapter-name',  chapter.name)
+					, { class: cidx === State.currentChapterIndex ? 'current' : '' }
+					, m('.label'
+						, m('h4.chapter-num', (isVideo ? cidx + '.' : ''))
+						, m('h4.chapter-name',  chapter.label)
+					)
+					, m('.selector'
+						, { onclick: State.setChapter.bind(null, cidx) }
+						, m('button.video', isVideo ? 'video' : 'home')
+						, m('button.practicum', 'practicum')
 					)
 				)
 
 			})
 			, m('button.expander.show-menu'
 				, {
-					class: State.firstRun_menu ? 'first-run' : '',
+					class: State.firstRun.menu ? 'first-run' : '',
 					onclick: State.toggleView.bind(null, 'menu')
 				}
 			)
