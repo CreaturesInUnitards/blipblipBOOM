207f1d716bc3dbc414fe0b69ea8adc4fed3dc39e
scotty <portrayme@mac.com>
2018-01-21 05:46:02 -0800

    INtroducing: Lab

---
+++ src/components/Lab/Lab.js
@@ -0,0 +1,44 @@
+/***********************************
+*
+* Lab
+*
+***********************************/
+const State = require('../../model/State')
+
+
+module.exports = {
+	view: () =>  {
+		const chapter = State.chapters[State.currentChapterIndex]
+		const flem = chapter.flems[State.currentFlemIndex]
+		
+		return m('section.lab'
+			, m('.resources'
+				, m('h5', 'Practicum')
+				, chapter.notes.split('|').map(function (note) {
+					return m('h4.notes', m.trust(note))
+				})
+				, m('ul.sandboxes'
+					, m('h5', 'Sandboxes')
+					, chapter.flems.map((fl, idx) => {
+						return m('h4'
+							, {
+								class: idx === State.currentFlemIndex ? 'current' : '',
+								onclick: State.setFlem.bind(null, idx)
+							}
+							, fl.label
+						)
+					})
+				)
+				, m('ul.links'
+					, m('h5', 'Resources')
+					, chapter.links.map((link) => {
+						return m('li', m('a[target=_blank]', { href: link.url }, link.text))
+					})
+				)
+			)
+			, m('.flems'
+				, flem && m('iframe', { src: 'http://tinyurl.com/' + flem.url })
+			)
+		)
+	}
+}
