c37d2d17b49376a1bf242bbc55acc7e0e55730f9
scotty <portrayme@mac.com>
2018-01-23 11:25:03 -0800

    add config to SiteLogo

--- src/components/SiteLogo/SiteLogo.js
+++ src/components/SiteLogo/SiteLogo.js
@@ -3,7 +3,13 @@ require('./SiteLogo.sass')
 module.exports = {
 	view: (vnode) => {
 		return m('.site-logo'
-			, { style: { fontSize: vnode.attrs.size || '1em' } }
+			, { 
+				style: { 
+					fontSize: vnode.attrs.size || '1em',
+					textAlign: vnode.attrs.align || 'center',
+					width: vnode.attrs.width || '100%'
+				} 
+			}
 			, m('i', 'm') 
 			, '('
 			, m('i'
