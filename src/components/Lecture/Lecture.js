a15b4ee5e90104763d47f695b77b172f1c1a452f
scotty <portrayme@mac.com>
2018-01-23 11:22:31 -0800

    add routing to .show-lab

--- src/components/Lecture/Lecture.js
+++ src/components/Lecture/Lecture.js
@@ -3,23 +3,33 @@
 * Lecture
 *
 ***********************************/
-const State = require('../../model/State')
 const Welcome = require('../Welcome/Welcome')
 const LoadingAnimation = require('../LoadingAnimation/LoadingAnimation')
 
+const toggleUrl = () => {
+	const cidx = State.currentChapterIndex
+	const view = State.labOpen ? 'content' : 'lab'
+	const fidx = State.currentFlemIndex
+	
+	return `/${cidx}/${view}/${fidx > 0 ? fidx : ''}`
+}
+
 module.exports = {
-	view: function (vnode) {
-		const welcome = vnode.attrs.welcomeVisible
+	view: function () {
+		const isWelcomeScreen = State.currentChapterIndex === 0
 		const canPlay = State.canPlay
+		
 		return m('section.lecture'
-			, !(welcome || canPlay) && m(LoadingAnimation)
-			, welcome
+			, !(isWelcomeScreen || canPlay) && m(LoadingAnimation)
+			, isWelcomeScreen
 				? m(Welcome)
 				: canPlay && m('#videoContainer')
-			, m('button.expander.show-lab'
+			, m('a.expander.show-lab'
 				, {
 					class: State.firstRun.lab ? 'first-run' : '',
-					onclick: vnode.attrs.expandFn.bind(null, 'lab')
+					href: toggleUrl(),
+					oncreate: m.route.link,
+					onupdate: m.route.link
 				}
 			)
 		)
