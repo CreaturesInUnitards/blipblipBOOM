8a4f2b52bdac80dcf7b5d1fd9c8b8c2acacee205
scotty <portrayme@mac.com>
2018-01-30 17:35:44 -0800

    foobar

--- src/components/Lecture/Lecture.js
+++ src/components/Lecture/Lecture.js
@@ -5,6 +5,7 @@
 ***********************************/
 const Welcome = require('../Welcome/Welcome')
 const LoadingAnimation = require('../LoadingAnimation/LoadingAnimation')
+let foo = 'bar'
 
 const toggleUrl = () => {
 	const cidx = State.currentChapterIndex
