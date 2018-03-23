f0bfa436d5e55dc51945393acf9f0f6a6f14a811
scotty <portrayme@mac.com>
2018-03-23 14:16:43 -0700

    ditched redundant method; added chapter cleanup to course removal

--- src/components/Auth/Operations.js
+++ src/components/Auth/Operations.js
@@ -1,21 +1,18 @@
-const EditObjectProperty = (collectionName, object, prop, newValue) => {
-	object[prop] = newValue
-	const saveObject = {}
-	saveObject[prop] = newValue
-	UpdateObject(collectionName, object.id, saveObject)
-}
-
 const UpdateObject = (collectionName, objectId, updateObj) => {
 	firebase.firestore().collection(collectionName).doc(objectId).update(updateObj)
 }
 
 const RemoveObject = (collectionName, objectId) => {
+	if (collectionName === 'courses') {
+		const chapters = AdminData.courses[objectId].data.chapters || []
+		chapters.forEach(id => { RemoveObject('chapters', id) })
+	} 
 	firebase.firestore().collection(collectionName).doc(objectId).delete()
 }
 
 
-const AddObject = (collectionName, addObj) => {
+const AddObject = (collectionName, addObj) => _e => {
 	firebase.firestore().collection(collectionName).add(addObj)
 }
 
-module.exports = { AddObject, UpdateObject, RemoveObject, EditObjectProperty }
+module.exports = { AddObject, UpdateObject, RemoveObject }
