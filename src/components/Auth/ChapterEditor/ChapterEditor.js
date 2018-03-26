6327d47fd581c62ba0ae31d613b2a72c8d24c573
scotty <portrayme@mac.com>
2018-03-26 14:19:27 -0700

    Introducing: flems/links dragsort

--- src/components/Auth/ChapterEditor/ChapterEditor.js
+++ src/components/Auth/ChapterEditor/ChapterEditor.js
@@ -12,10 +12,11 @@ const UpdateObject = require('../Operations').UpdateObject
 /*********************** GLOBAL AdminData ***********************/
 
 let editing, currentFlem, chapter = null
+let dnd = { drag: null, drop: null }
 
 const addChild = prop => _e => { 
 	chapter.data[prop] = (chapter.data[prop] || [])
-		.concat({ label: `New ${prop.slice(0, prop.length - 1)}` }) 
+		.concat({ id: Date.now(), label: `New ${prop.slice(0, prop.length - 1)}`, url: '' }) 
 }
 
 const cancel = _e => { chapter = null }
@@ -33,6 +34,44 @@ const deleteChild = (array, idx) => _e => { chapter.data[array].splice(idx, 1) }
 const windowClick = e => {
 	// e.stopPropagation()
 }
+
+const dndClass = (item, current) => {
+	const classes = []
+	if (item === dnd.drag) classes.push('dragging')
+	if (item === dnd.drop) classes.push('dropping')
+	if (item === current) classes.push('current')
+	return classes.join(' ')
+}
+
+
+const dragAttrs = (o, array, current) => ({
+	key: o.id,
+	class: dndClass(o, current),
+	ondragstart: _e => { dnd.drag = o },
+	ondragover: _e => { if (dnd.drag && array.indexOf(o) > -1) dnd.drop = o },
+	ondragleave: _e => { dnd.drop = null },
+	ondragend: _e => { dnd = { drag: null, drop: null } }
+})
+
+const dragover = e => { e.preventDefault() }
+const drop = (array, saveChanges) => e => {
+	e.stopPropagation()
+	if (!(dnd.drag && dnd.drop)) return
+	const draggedIdx = array.indexOf(dnd.drag)
+	const droppedIdx = array.indexOf(dnd.drop)
+
+	const insertionIdx = draggedIdx < droppedIdx ? droppedIdx + 1 : droppedIdx
+	const deletionIdx = draggedIdx > droppedIdx ? draggedIdx + 1 : draggedIdx
+
+	if (insertionIdx !== deletionIdx) {
+		// your custom  code for updating the list goes here.
+		array.splice(insertionIdx, 0, dnd.drag)
+		array.splice(deletionIdx, 1)
+
+		saveChanges()
+	}
+}
+
 
 module.exports = v => {
 	return {
@@ -87,67 +126,75 @@ module.exports = v => {
 							m('h3.mra', 'Exercises'),
 							m('button.add-button.font-24', { onclick: addChild('flems') }, '⊕')
 						),
-						obj.flems && obj.flems.map((flem, idx) => {
-							const isCurrent = flem === currentFlem
-							const n = idx + 1
-						
-							return m('.flem',
-								{ class: isCurrent ? 'current' : '' },
-								isCurrent
-									? m('.current-box.p10.bg-dark.mt20.c-white',
-										m('.font-12.mb20', n),
-										m('.top-fields.flex.ac',
-											m(FormField, {
-												labelClass: 'mb6',
-												inputClass: 'p6 font-16 w100pct',
-												type: 'text',
-												placeholder: 'Flem Title',
-												label: 'Flem Title',
-												value: flem.label,
-												oninput: v => flem.label = v
-											}),
-											m(FormField, {
-												class: 'ml20',
-												labelClass: 'mb6',
-												inputClass: 'p6 font-16 w100pct',
-												type: 'text',
-												placeholder: 'not including https://tinyurl.com/',
-												label: 'Tinyurl',
-												value: flem.url,
-												oninput: v => { flem.url = v.subtr(0, 8) }
-											}),
-											m(FormField, {
-												class: 'ml20',
-												labelClass: 'mb6',
-												inputClass: 'p6 font-16 w100pct',
-												type: 'text',
-												placeholder: '...if any',
-												label: 'Cue Stop',
-												value: flem.cuestop,
-												oninput: v => {
-													const n = parseInt(v)
-													flem.cuestop = (typeof n == 'number' && n == n) ? n : ''
-												}
-											})
-										),
-										m('.notes-box.mt20.bg-light.c-dark.p10',
-											m('h3.mb6', 'Notes'),
-											m('.p20.bg-grey.c-white', m.trust(flem.notes)),
-											m('.pt10.flex.ac', 
-												m('button.bg-dark.c-white.brad4.mla', 
-													{ onclick: _e => { editing = flem } }, 
-													'Edit Notes'
-												)
-											)											
-										)
-									)
-									: m('.box.mt20.p10.b1-black.bg-white.flex.ac.pointer',
-										m('.font-18.mra', `${n}: ${flem.label}`),
-										m('button.edit-button.c-light.font-24', { onclick: _e => ( currentFlem = flem ) }, '✎'),
-										m('button.delete-button.c-light.font-24', { onclick: deleteChild('flems', idx) }, '⊗')
-									) 
+						m('',
+							{
+								ondragover: dragover,
+								ondrop: drop(obj.flems, () => { })
+							},
+							obj.flems && obj.flems.map((flem, idx) => {
+								const isCurrent = flem === currentFlem
+								const n = idx + 1
 
-							)}
+								return m('.drag-item[draggable]',
+									dragAttrs(flem, obj.flems, currentFlem),
+									m('.flem',
+										isCurrent
+											? m('.current-box.p10.mt20.c-white.bg-dark',
+												m('.font-12.mb20', n),
+												m('.top-fields.flex.ac',
+													m(FormField, {
+														labelClass: 'mb6',
+														inputClass: 'p6 font-16 w100pct',
+														type: 'text',
+														placeholder: 'Flem Title',
+														label: 'Flem Title',
+														value: flem.label,
+														autofocus: true,
+														oninput: v => { flem.label = v }
+													}),
+													m(FormField, {
+														class: 'ml20',
+														labelClass: 'mb6',
+														inputClass: 'p6 font-16 w100pct',
+														type: 'text',
+														placeholder: 'not including https://tinyurl.com/',
+														label: 'Tinyurl',
+														value: flem.url,
+														oninput: v => { flem.url = v.substr(0, 8) }
+													}),
+													m(FormField, {
+														class: 'ml20',
+														labelClass: 'mb6',
+														inputClass: 'p6 font-16 w100pct',
+														type: 'text',
+														placeholder: '...if any',
+														label: 'Cue Stop',
+														value: flem.cuestop,
+														oninput: v => {
+															const n = parseInt(v)
+															flem.cuestop = (typeof n == 'number' && n == n) ? n : ''
+														}
+													})
+												),
+												m('.notes-box.mt20.bg-light.c-dark.p10',
+													m('h3.mb6', 'Notes'),
+													m('.p20.bg-grey.c-white', m.trust(flem.notes)),
+													m('.pt10.flex.ac',
+														m('button.bg-dark.c-white.brad4.mla',
+															{ onclick: _e => { editing = flem } },
+															'Edit Notes'
+														)
+													)
+												)
+											)
+											: m('.box.mt20.p10.b1-black.flex.ac.pointer',
+												m('.font-18.mra', flem.label),
+												m('button.edit-button.c-light.font-24', { onclick: _e => ( currentFlem = flem ) }, '✎'),
+												m('button.delete-button.c-light.font-24', { onclick: deleteChild('flems', idx) }, '⊗')
+											)
+
+								))
+							})
 						),
 						editing && m(NotesEditor, { flem: editing, done: v => {
 							editing.notes = v
@@ -159,40 +206,47 @@ module.exports = v => {
 							m('h3.mra', 'Links'),
 							m('button.add-button.font-24', { onclick: addChild('links') }, '⊕')
 						),
-						obj.links && obj.links.map((link, idx) => {
-							return m('.link.p6.mt20.bg-white',
-								m('.flex', 
-									m('button.delete-button.c-light.font-24.mla', { onclick: deleteChild('links', idx) }, '⊗')
-								),
-								m('.top-fields.flex.ac',
-									m(FormField, {
-										labelClass: 'mb6',
-										inputClass: 'p6 font-16 w100pct',
-										type: 'text',
-										placeholder: 'Link Text',
-										label: 'Link Text',
-										value: link.label,
-										oninput: v => link.label = v
-									}),
-									m(FormField, {
-										class: 'ml20',
-										labelClass: 'mb6',
-										inputClass: 'p6 font-16 w100pct',
-										type: 'text',
-										placeholder: 'Link URL',
-										label: 'Link URL',
-										value: link.url,
-										oninput: v => { link.url = v }
-									})
+						m('',
+							{
+								ondragover: dragover,
+								ondrop: drop(obj.links, () => { })
+							},
+							obj.links && obj.links.map((link, idx) => {
+								return m('.drag-item[draggable]',
+									dragAttrs(link, obj.links, null),
+									m('.link.p6.mt20',
+										m('.flex',
+											m('.font-12.mb20', idx + 1),
+											m('button.delete-button.c-light.font-24.mla', { onclick: deleteChild('links', idx) }, '⊗')
+										),
+										m('.top-fields.flex.ac',
+											m(FormField, {
+												labelClass: 'mb6',
+												inputClass: 'p6 font-16 w100pct',
+												type: 'text',
+												placeholder: 'Link Text',
+												label: 'Link Text',
+												value: link.label,
+												oninput: v => link.label = v
+											}),
+											m(FormField, {
+												class: 'ml20',
+												labelClass: 'mb6',
+												inputClass: 'p6 font-16 w100pct',
+												type: 'text',
+												placeholder: 'Link URL',
+												label: 'Link URL',
+												value: link.url,
+												oninput: v => { link.url = v }
+											})
+										)
+									)
 								)
-							)
-						})
+							})
+						)
 					)
 				)
 			)
 		}
 	}
 }
-
-
-// TODO: dragsort flems & links (abstract the array of children from DNDList.js)
