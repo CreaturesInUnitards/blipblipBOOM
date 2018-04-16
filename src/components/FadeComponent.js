df39238b2160c3262a97c1eff5fa2dbb8b019b89
scotty <portrayme@mac.com>
2018-04-16 12:30:07 -0700

    hardwired FadeComponent

--- src/components/FadeComponent.js
+++ src/components/FadeComponent.js
@@ -1,12 +1,12 @@
 module.exports = module.exports = {
 	oncreate: ({ dom, attrs }) => {
-		if (attrs.fadein) dom.classList.add(attrs.enterClass || 'enter')
+		dom.classList.add('enter')
 		if (attrs.onupdate) {
 			this.onupdate = this.oncreate
 		}
 	},
-	onbeforeremove: ({ dom, attrs }) => {
-		dom.classList.add(attrs.exitClass || 'exit')
+	onbeforeremove: ({ dom }) => {
+		dom.classList.add('exit')
 		return new Promise((resolve) => {
 			setTimeout(resolve, 800)
 		})
