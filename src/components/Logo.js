3e44fde2176ca31d6d84107ae00f3dbe88fd4f3f
scotty <portrayme@mac.com>
2018-04-16 11:20:04 -0700

    moved logo to m.route.link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
-/* global m, State, Actions */
+/* global m */
 module.exports = {
-	view: _v => m('.logo.fix.flex.jc.ac.w40.h40.bot0.l10.pointer',
-		{ onclick: _e => { m.route.set('/') } },
+	view: _v => m('a.logo.fix.flex.jc.ac.w40.h40.bot0.l10.pointer[href=/]',
+		{ oncreate: m.route.link },
 		m('.blip-wrapper.rel.flex.ja.ae.w100p.h50p', 
 			m('.blip.in-block.bg-brick.rad50p.w20p.h40p'),
 			m('.blip.in-block.bg-brick.rad50p.w20p.h40p'),
