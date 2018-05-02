1daffc97d2b46eade9c38df66aeacb1eea993fad
scotty <portrayme@mac.com>
2018-05-01 18:22:04 -0700

    comma-first coup d'etat

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const FadeComponent = require('./FadeComponent')
 const HowToUseThisSite = require('./HowToUseThisSite')
 
 // big shoutout to @porsager for enabling this disabling :) 
-const sendMessage = _e => {
+const disableFullscreenFlems = _e => {
 	setTimeout(() => {
 		document.getElementById('flemFrame').contentWindow.postMessage({
 			name: 'assign',
@@ -114,127 +114,143 @@ module.exports = _v => {
 
 			return State.courses && State.chapters && State.chapters.length
 				? [
-					m('main.fix.f1.flex.jc.bg-dark.vw100.vh100',
+					m('main.fix.f1.flex.jc.bg-dark.vw100.vh100'
 						
 						// sandbox 
-						m('#sandbox.fix.w100p.vh100.pl60.t0.l0.flex',
-							
-							// exercises/resources panel
-							m('.w240.bg-panel.flex.col.jb.h100p',
+						, m('#sandbox.fix.w100p.vh100.pl60.t0.l0.flex'							
+					
+									// exercises/resources panel
+							, m('.w240.bg-panel.flex.col.jb.h100p'
 								
 								// exercises
-								m('.oa',
-									m('.flex.jc.ac.c-white.h38', m('span.fw7.fs20', 'EXERCISES')),
-									m('',
-										chapter.flems.map((aFlem, idx) => m('.rel.flem.bs-5-dark',
-											{
+								, m('.oa'
+									, m('.flex.jc.ac.c-white.h38', m('span.fw7.fs20', 'EXERCISES'))
+									, m(''
+										, chapter.flems.map((aFlem, idx) => m('.rel.flem.bs-5-dark'
+											, {
 												key: flem.id,
 												class: aFlem === flem ? 'bg-green c-white' : 'bg-light c-dark',
 												style: { zIndex: chapter.flems.length - idx },
-											},
-											m('.flem-label.p10-20.fw7.pointer.flex.ac.tr3',
+											}
+											, m('.flem-label.p10-20.fw7.pointer.flex.ac.tr3',
 												{ onclick: flemNotesOut(idx) },
 												`${idx + 1}. ${aFlem.label}`
-											),
-											aFlem === flem && m('.notes.bg-white.c-dark.bt-1-dark.w100.oh.none',
-												{ oncreate: flemNotesIn },
-												m('.p20.lh15', m.trust(aFlem.notes))
+											)
+											,aFlem === flem && m('.notes.bg-white.c-dark.bt-1-dark.w100.oh.none'
+												, { oncreate: flemNotesIn }
+												, m('.p20.lh15', m.trust(aFlem.notes))
 											)
 										))
 									)
-								),
+								)
 								
 								// resources
-								m('.pb20',
-									m('.flex.jc.ac.c-white.h38', m('span.fw7.fs20', 'RESOURCES')),
-									chapter.links.map(link => m('.p5-20',
-										m('a.c-white.underline.fs08[target=_blank]',
-											{ href: link.url },
-											link.label
+								, m('.pb20'
+									, m('.flex.jc.ac.c-white.h38', m('span.fw7.fs20', 'RESOURCES'))
+									, chapter.links.map(link => m('.p5-20'
+										, m('a.c-white.underline.fs08[target=_blank]'
+											, { href: link.url }
+											, link.label
 										))
 									)
 								)
-							),
+							)
 							
 							// flemsesezz
-							m('.flems.f2.rel.h100p',
-								State.flemReady
-									? m(FadeComponent, { key: 'flem' }, m('iframe#flemFrame.w100p.h100p.bg-white', {
-										onload: sendMessage, // disable fullscreen flemses
-										src: `https://tinyurl.com/${flem.url }`
-										// TODO: ditch tinyurl once @porsager ships persistence
-									})) 
-									: m(FadeComponent, { key: 'fade' }, m('.abs.t0.l0.w100p.h100p.bg-dark', m(LoadingAnimation, 'loading exercise...')))
-							),
-						),
+							, m('.flems.f2.rel.h100p'
+								, State.flemReady
+									? m(FadeComponent
+										, { key: 'flem' }
+										, m('iframe#flemFrame.w100p.h100p.bg-white'
+											, {
+												onload: disableFullscreenFlems, 
+												src: `https://tinyurl.com/${flem.url }`
+												// TODO: ditch tinyurl once @porsager ships persistence
+											}
+										)
+									) 
+									: m(FadeComponent
+										, { key: 'fade' }
+										, m('.abs.t0.l0.w100p.h100p.bg-dark'
+											, m(LoadingAnimation, 'loading exercise...')
+										)
+									)
+							)
+						)
 						
 						// video player
-						m('#video.fix.w100p.vh100.t0.l0.pl60.trVid.oh.bg-dark',
-							{ class: isSandbox ? 'transY-100' : ''},
-							m('#videoContainer.rel.t0.w100p.vh100.bg-dark.flex.jc.ac.o0'),
+						, m('#video.fix.w100p.vh100.t0.l0.p0-60.trVid.oh.bg-dark'
+							, { class: isSandbox ? 'transY-100' : ''}
+							, m('#videoContainer.rel.t0.w100p.vh100.bg-dark.flex.jc.ac.o0')
 						)
-					),
+					)
 					
 					// sandbox/video toggler
-					routedLink('a.switch.fix.l10.t80.w40.h64.rad20x', {
-						class: isSandbox ? 'S' : 'V',
-						href: Actions.toggledUrl(),
-					}),
+					, routedLink('a.switch.fix.l10.t80.w40.h64.rad20x'
+						, {
+							class: isSandbox ? 'S' : 'V',
+							href: Actions.toggledUrl()
+						}
+					)
 					
 					// menu overlay
-					menuOpen && m('.fix.t0.l0.bg-black.o8.vw100.vh100.fade-in',
-						{
+					, menuOpen && m('.fix.t0.l0.bg-black.o8.vw100.vh100.fade-in'
+						, {
 							onclick: _e => { menuOpen = false },
 							onbeforeremove: menuExitAnimation('fade-out', 300)
 						}
-					),
+					)
 					
 					// menu
-					menuOpen && m('menu.fix.bg-dark.t0.l0.w240.vh100.oa.p2x.pt60.flex.col.ae.slide-in',
-						{
+					, menuOpen && m('menu.fix.bg-dark.t0.l0.w240.vh100.oa.p2x.pt60.flex.col.ae.slide-in'
+						, {
 							onclick: _e => { menuOpen = false },
 							onbeforeremove: menuExitAnimation('slide-out', 300)
-						},
+						}
 						// TODO: Masthead here
-						// m('.abs.c-white', State.courses[courseID].data.title),
-						State.chapters.map((chapter, idx) => {
+						, State.chapters.map((chapter, idx) => {
 							const isCurrent = chapterIdx === idx
-							return m('.list-item.flex.jb.ac.w100p.bs-2-black',
-								{
+							return m('.list-item.flex.jb.ac.w100p.bs-2-black'
+								, {
 									class: isCurrent ? 'c-green' : 'c-white',
 									style: { zIndex: State.chapters.length - idx }
-								},
+								}
 								
 								// chapter title text
-								routedLink('a.f2.p10.fs20', { href: `/${courseID}/${idx}` }, chapter.title),
+								, routedLink('a.f2.p10.fs20', { href: `/${courseID}/${idx}` }, chapter.title)
 								
 								// chapter video/sandbox buttons
-								m('.mla.w40.h64.flex.col.pointer',
-									routedLink('.w40.h32.bgs-cover.bgp-top',
-										{
+								, m('.mla.w40.h64.flex.col.pointer'
+									, routedLink('.w40.h32.bgs-cover.bgp-top'
+										, {
 											href: `/${courseID}/${idx}/video`,
 											class: !isSandbox && isCurrent ? 'bg-yah' : 'bg-off'
 										}
-									),
-									routedLink('.w40.h32.bgs-cover.bgp-bot',
-										{
+									)
+									, routedLink('.w40.h32.bgs-cover.bgp-bot'
+										, {
 											href: `/${courseID}/${idx}/sandbox`,
 											class: isSandbox && isCurrent ? 'bg-yah' : 'bg-off'
 										}
 									)
-								),
+								)
 							)
-						}),
-					),
-					m('.menuButton.fix.t10.l10.w40.h40.pointer.tr3',
-						{
+						})
+					)
+					, m('.menuButton.fix.t10.l10.w40.h40.pointer.tr3'
+						, {
 							class: menuOpen ? 'O l240' : 'C l0',
 							onclick: toggleMenu
 						}
-					),
-					m('a[href=/dashboard]', { oncreate: m.route.link }, m(Logo, { class: 'fix w40 h20 bot10 l10' })),
-					!State.canPlay && m(FadeComponent, m('.abs.t0.l0.w100p.h100p.bg-dark', m(LoadingAnimation, 'loading chapter...'))),
-					(wSize.w < wSize.h) && m(HowToUseThisSite)
+					)
+					, m('a[href=/]'
+						, { oncreate: m.route.link }
+						, m(Logo, { class: 'fix w40 h20 bot10 l10' })
+					)
+					, !State.canPlay && m(FadeComponent
+						, m('.abs.t0.l0.w100p.h100p.bg-dark', m(LoadingAnimation, 'loading chapter...'))
+					)
+					, (wSize.w < wSize.h) && m(HowToUseThisSite)
 				]
 				: m(LoadingAnimation, 'loading course data...')
 		}
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 const blipColor = color => `bg-${color || 'brick'}`
 
 module.exports = {
-	view: ({attrs}) => m('.logo.flex.jc.ac',
-		attrs,
-		m('.blip-wrapper.rel.flex.ja.ae.w100p.h100p', 
-			m('.blip.in-block.rad50p.w20p.h40p', { class: blipColor(attrs.blipColor) }),
-			m('.blip.in-block.rad50p.w20p.h40p', { class: blipColor(attrs.blipColor) }),
-			m('.blip.in-block.rad50p.w50p.h100p', { class: blipColor(attrs.blipColor) })
+	view: ({attrs}) => m('.logo.flex.jc.ac'
+		, attrs
+		, m('.blip-wrapper.rel.flex.ja.ae.w100p.h100p'
+			, m('.blip.in-block.rad50p.w20p.h40p', { class: blipColor(attrs.blipColor) })
+			, m('.blip.in-block.rad50p.w20p.h40p', { class: blipColor(attrs.blipColor) })
+			, m('.blip.in-block.rad50p.w50p.h100p', { class: blipColor(attrs.blipColor) })
 		)
 	)	
 }
